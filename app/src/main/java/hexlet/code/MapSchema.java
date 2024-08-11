aaf065171bce6d87eec94196a4ec16497880de91
skavicos <kskavinsky@skolopendra.com>
2024-08-11 18:47:45 +0300

    add step 4

---
+++ app/src/main/java/hexlet/code/MapSchema.java
@@ -0,0 +1,26 @@
+package hexlet.code;
+
+import java.util.Map;
+
+public final class MapSchema extends BaseSchema {
+    public MapSchema required() {
+        getPredicates().add(v -> v instanceof Map<?, ?>);
+        return this;
+    }
+
+    public MapSchema sizeof(int size) {
+        getPredicates().add(v -> ((Map<?, ?>) v).size() == size);
+        return this;
+    }
+
+    boolean checkMap(Map<?, ?> v, Map<?, BaseSchema> sch) {
+        for (var x : sch.entrySet()) {
+            var k = x.getKey();
+            var value = x.getValue();
+            if (!value.isValid(v.get(k))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
