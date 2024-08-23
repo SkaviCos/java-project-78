c6848aeb2dd0d109c4a7a9a69234626b8c64e48d
skavicos <kskavinsky@skolopendra.com>
2024-08-23 09:42:39 +0300

    fix imports

--- app/src/test/java/ValidatorTest.java
+++ app/src/test/java/ValidatorTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ValidatorTest {
 
