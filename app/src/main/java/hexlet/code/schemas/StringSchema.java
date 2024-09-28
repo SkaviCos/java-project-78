53bc7d39cbc7cbf2d5759839fafda2c0163b1d1d
skavicos <kskavinsky@skolopendra.com>
2024-09-28 21:45:43 +0300

    revert some changes

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -8,19 +8,12 @@ public final class StringSchema extends BaseSchema<String> {
     }
 
     public StringSchema minLength(int length) {
-        super.predicatesList(v -> v.length() >= length);
-//        super.predicatesList(v -> v == null || v.length() >= length);
-//        при добавлении условия выше падает тест в пайпе hexlet-check
-//         ValidatorTest > testStringValidator() FAILED
-//        app-1  |     org.opentest4j.AssertionFailedError:
-//        app-1  |     Expecting value to be true but was false
-
+        super.predicatesList(v -> v == null || v.length() >= length);
         return this;
     }
 
     public StringSchema contains(String str) {
-        super.predicatesList(v -> v.contains(str));
-//        super.predicatesList(v -> v == null || v.contains(str));
+        super.predicatesList(v -> v == null || v.contains(str));
         return this;
     }
 
