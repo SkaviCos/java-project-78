b89ccf3a4119f0ed5047c97c90f9c87c931c9fdb
skavicos <kskavinsky@skolopendra.com>
2024-09-28 21:36:05 +0300

    revert some changes

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -10,16 +10,11 @@ public final class StringSchema extends BaseSchema<String> {
     public StringSchema minLength(int length) {
         super.predicatesList(v -> v.length() >= length);
 //        super.predicatesList(v -> v == null || v.length() >= length);
-        /**
-         * при добавлении условия выше падает тест в пайпе hexlet-check
-         *  ValidatorTest > testStringValidator() FAILED
-         * app-1  |     org.opentest4j.AssertionFailedError:
-         * app-1  |     Expecting value to be true but was false
-         * app-1  |         at java.base@21.0.3/jdk.internal.reflect.DirectConstructorHandleAccessor.newInstance(DirectConstructorHandleAccessor.java:62)
-         * app-1  |         at java.base@21.0.3/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:502)
-         * app-1  |         at app//hexlet.project.ValidatorTest.testStringValidator(ValidatorTest.java:41)
-         * app-1  |
-         */
+//        при добавлении условия выше падает тест в пайпе hexlet-check
+//         ValidatorTest > testStringValidator() FAILED
+//        app-1  |     org.opentest4j.AssertionFailedError:
+//        app-1  |     Expecting value to be true but was false
+
         return this;
     }
 
